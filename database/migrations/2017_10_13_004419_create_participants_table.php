730fe33d63cb8f19e065d7fd88328ef79642aee7
Sander Borret <sander.borret@student.kdg.be>
2017-10-13 03:09:17 +0200

    add participants migration and model

---
+++ database/migrations/2017_10_13_004419_create_participants_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('address', 100);
+            $table->string('city', 100);
+            $table->string('email', 100)->unique();
+            $table->string('ip')->unique();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
