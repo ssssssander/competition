80cd08edf56583a1ae9db642f2f7e0dbb252e158
Sander Borret <sander.borret@student.kdg.be>
2017-10-13 20:01:36 +0200

    tweak participant validation

--- app/Forms/ParticipantForm.php
+++ app/Forms/ParticipantForm.php
@@ -22,7 +22,7 @@ class ParticipantForm extends Form
                 'rules' => 'required|max:100|min:5|email|unique:participants,email|string', 'label'=>'E-mailadres', 'errors' => ['class' => 'text-danger']
             ])
             ->add('image', 'file', [
-                'rules' => 'required|max:3000|image|mimes:jpeg,jpg,png|dimensions:min_width=100,min_height=200', 'label'=>'Jouw foto (max. 3MB, min. 100x200)', 'errors' => ['class' => 'text-danger']
+                'rules' => 'required|max:5000|image|mimes:jpeg,jpg,png|dimensions:min_width=100,min_height=200', 'label'=>'Jouw foto (max. 5MB, min. 100x200)', 'errors' => ['class' => 'text-danger']
             ])
             ->add('submit', 'submit', [
                 'label' => 'Verzenden!', 'attr' => ['class' => 'btn btn-lg btn-default btn-block text-uppercase'], 'errors' => ['class' => 'text-danger']
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,11 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <p>Dashboard</p>
+                <div class="panel panel-default">
+                    <div class="panel-heading">Dashboard</div>
+                    <div class="panel-body">
+                        Lijst deelnemers + soft delete
+                    </div>
             </div>
         </div>
     </div>
