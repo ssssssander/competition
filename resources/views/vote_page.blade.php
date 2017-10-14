5a19ecdd0cd20d0faa55206579fd7aafa6d7bb50
Sander Borret <sander.borret@student.kdg.be>
2017-10-14 23:09:08 +0200

    fix vote view style

--- resources/views/vote_page.blade.php
+++ resources/views/vote_page.blade.php
@@ -5,11 +5,11 @@
 @section('content')
     <p class="lead">Stem op jouw favoriete foto!</p>
     @forelse ($participants as $participant)
-        <div class="col-md-4 thumbnail">
+        <div class="thumbnail pull-left">
             <img src="{{ asset($participant->image_path) }}" alt="{{ $participant->name }}">
-            <div class="caption text-center">
+            <div class="caption">
                 <small>{{ $participant->name }}</small>
-                <p>{{ $participant->votes }} @if($participant->votes == 1) stem @else stemmen @endif</p>
+                <p class="d-inline-block">{{ $participant->votes }} @if($participant->votes == 1) stem @else stemmen @endif</p>
             </div>
         </div>
     @empty
