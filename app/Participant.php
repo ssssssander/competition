6c004c007bc0bd6c71a1561750306de6f2bc93c1
Sander Borret <sander.borret@student.kdg.be>
2017-11-04 04:13:49 +0100

    remove fonts folder

--- app/Participant.php
+++ app/Participant.php
@@ -4,11 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Kyslik\ColumnSortable\Sortable;
 
 class Participant extends Model
 {
-    use SoftDeletes, Sortable;
+    use SoftDeletes;
 
     protected $fillable = ['name', 'address', 'city', 'email'];
 }
